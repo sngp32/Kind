258921424aa26175244dfb31572cb71335583d1a
SebastianHalfdanLauridsen <s215769@dtu.dk>
2022-11-21 16:41:11 +0100

    Created Mypage.kt layout

--- app/src/main/java/com/example/kind/view/Mypage.kt
+++ app/src/main/java/com/example/kind/view/Mypage.kt
@@ -2,11 +2,9 @@ package com.example.kind.view
 
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,34 +13,73 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import com.example.kind.ui.theme.Shapes
 
 @Composable
 fun MypageScreen(/*navController: NavController*/) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFB8E3AD)),
+            ) {
+                Text(
+                    text = "Min side",
+                    fontSize = 32.sp,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(top = 30.dp, bottom = 30.dp)
+                )
+            }
+        },
+        //TODO bottom navigation bar missing?
+        bottomBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFB8E3AD))
+                    .padding(top = 50.dp, bottom = 50.dp)
+            ) {
+                //empty
+            }
+        }
     ) {
+        //this box has to exist for some reason
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color(0xFFB8E3AD))
-                .padding(top = 30.dp, bottom = 30.dp), contentAlignment = Alignment.Center
+                .fillMaxHeight()
         ) {
-            Text(
-                text = "Min side",
-                fontSize = 32.sp,
-            )
-
-        }
-        Box() {
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(text = "Min Portefølje")
+            Column(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+            ) {
+                Spacer(modifier = Modifier.size(60.dp))
+                Surface(
+                    shape = Shapes.medium,
+                    elevation = 2.dp,
+                    modifier = Modifier.clickable { /* TODO navcontroller here?*/ }) {
+                    Text(
+                        text = "Min Portefølje",
+                        fontSize = 32.sp,
+                        modifier = Modifier
+                            .padding(all = 20.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.size(60.dp))
+                Surface(
+                    shape = Shapes.medium,
+                    elevation = 2.dp,
+                    modifier = Modifier.clickable { /* TODO navcontroller here?*/ }) {
+                    Text(
+                        text = "Indstillinger",
+                        fontSize = 32.sp,
+                        modifier = Modifier
+                            .padding(all = 20.dp)
+                    )
+                }
             }
-
-        }
-        Box() {
-            Text(text = "Indstillinger")
         }
     }
 }
