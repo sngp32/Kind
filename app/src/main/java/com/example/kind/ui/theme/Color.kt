2a2a8035345e4bd3f8dc86e4178d669868a27884
KaNN <lasseg123@hotmail.com>
2022-11-22 17:58:24 +0100

    BIGCHUNK

--- app/src/main/java/com/example/kind/ui/theme/Color.kt
+++ app/src/main/java/com/example/kind/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.example.kind.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple500 = Color(0xFF5D8555)
+val Purple700 = Color(0xFF5D8555)
+val Teal200 = Color(0xFFB8E3AD)
