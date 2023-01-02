61ab7db5d93db00b9d67ebad5fd1b0912d3e48f3
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-02 17:07:16 +0100

    Add predefined navigation route destinations
    To navigate in the app we are defining the routes as constants.
    
    These will be used in a future KindNavigationActions class
    to navigate between screens.
    
    Example usage of the destinations:
    
      navController.navigate(KindDestinations.HOME_ROUTE) { /* ... */}

---
+++ app/src/main/java/com/example/kind/controller/KindNavigation.kt
@@ -0,0 +1,14 @@
+package com.example.kind.controller
+
+/**
+ * Destinations used in the Kind app
+ */
+object KindDestinations {
+    const val HOME_ROUTE = "home"
+    const val LOGIN_ROUTE = "login"
+    const val SIGNUP_ROUTE = "signup"
+    const val MYPAGE_ROUTE = "mypage"
+    const val SETPORTFOLIO_ROUTE = "setportfolio"
+    const val PORTFOLIO_ROUTE = "portfolio"
+    const val SETTINGS_ROUTE = "settings"
+}
