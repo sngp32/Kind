7a61a6d6da93255af4289efabfe4476599d7bd9e
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-04 15:24:39 +0100

    Change app launch and overall navigation
    The MainActivity now launches only the KindApp
    without theme surrounding it (this is pushed
    in KindApp).
    
    The bottom navigation bar will now always be
    on every page but this is contained in the
    highest caller (KindApp).
    
    The navigationHostController has been reworked
    to follow best practices.

---
+++ app/src/main/java/com/example/kind/KindApp.kt
@@ -0,0 +1,74 @@
+package com.example.kind
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.kind.navigation.*
+import com.example.kind.ui.theme.KindTheme
+import com.example.kind.view.AppNavBar
+import com.example.kind.view.HomeScreen
+import com.example.kind.view.myPage.MyPageScreen
+import com.example.kind.view.setPortfolio.SetPortfolioScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KindApp() {
+    KindTheme {
+        val navController = rememberNavController()
+
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+
+        val currentScreen =
+            kindBottomBarScreens.find { it.route == currentDestination?.route } ?: Home
+
+        Scaffold(
+            topBar = {},
+            bottomBar = {
+                AppNavBar(
+                    allScreens = kindBottomBarScreens,
+                    onTabSelected = { newScreen ->
+                        navController.navigateSingleTopTo(newScreen.route)
+                    },
+                    currentScreen = currentScreen
+                )
+            }
+        ) { innerPadding ->
+            KindNavHost(navController = navController, modifier = Modifier.padding(innerPadding))
+        }
+        KindNavGraph(navController = navController)
+    }
+}
+
+@Composable
+fun KindNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Home.route,
+        modifier = modifier
+    ) {
+        composable(route = Home.route) {
+            HomeScreen()
+        }
+        composable(route = SetPortfolio.route) {
+            SetPortfolioScreen()
+        }
+        composable(route = MyPage.route) {
+            MyPageScreen()
+        }
+    }
+}
+
+fun NavHostController.navigateSingleTopTo(route: String) =
+    this.navigate(route) { launchSingleTop = true }
--- app/src/main/java/com/example/kind/MainActivity.kt
+++ app/src/main/java/com/example/kind/MainActivity.kt
@@ -3,17 +3,12 @@ package com.example.kind
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.kind.controller.Navigation
-import com.example.kind.ui.theme.KindTheme
-import com.example.kind.view.SettingsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KindTheme {
-              Navigation()
-            }
+            KindApp()
         }
     }
 }
