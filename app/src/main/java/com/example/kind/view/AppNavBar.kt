0c0431c7a07c4639b5f86c4cad2bf0357d49bfe0
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-04 15:20:54 +0100

    Change bottom navigation bar to be stateless
    The bottom navigation bar will now get all
    screens, the current screen and push the
    action of selecting a screen upwards with
    onTabSelected.

--- app/src/main/java/com/example/kind/view/AppNavBar.kt
+++ app/src/main/java/com/example/kind/view/AppNavBar.kt
@@ -1,63 +1,33 @@
 package com.example.kind.view
 
 import android.content.res.Configuration
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kind.controller.KindDestinations
+import com.example.kind.navigation.Home
+import com.example.kind.navigation.KindBottomBarDestination
+import com.example.kind.navigation.KindDestination
+import com.example.kind.navigation.kindBottomBarScreens
 import com.example.kind.ui.theme.KindTheme
 
 @Composable
 fun AppNavBar(
-    currentRoute: String,
-    navigateToHome: () -> Unit,
-    navigateToSetPortfolio: () -> Unit,
-    navigateToMyPage: () -> Unit,
-    modifier: Modifier = Modifier
+    allScreens: List<KindBottomBarDestination>,
+    onTabSelected: (KindDestination) -> Unit,
+    currentScreen: KindDestination
 ) {
     NavigationBar(
-        modifier = modifier,
         containerColor = BottomAppBarDefaults.containerColor, //TODO MaterialTheme
-        contentColor = contentColorFor(BottomAppBarDefaults.containerColor) //TODO MaterialTheme
+        contentColor = contentColorFor(BottomAppBarDefaults.containerColor), //TODO MaterialTheme
     ) {
-        NavigationBarItem(
-            selected = currentRoute == KindDestinations.HOME_ROUTE,
-            onClick = navigateToHome,
-            icon = {
-                Icon(
-                    Icons.Filled.Home,
-                    contentDescription = null //TODO create string resources for titles
-                )
-            },
-            label = { Text("Text") } //TODO create string resources for labels
-        )
-        NavigationBarItem(
-            selected = currentRoute == KindDestinations.SETPORTFOLIO_ROUTE,
-            onClick = navigateToSetPortfolio,
-            icon = {
-                Icon(
-                    Icons.Filled.Favorite,
-                    contentDescription = null //TODO create string resources for titles
-                )
-            },
-            label = { Text("Text") } //TODO create string resources for labels
-        )
-        NavigationBarItem(
-            selected = currentRoute == KindDestinations.MYPAGE_ROUTE,
-            onClick = navigateToMyPage,
-            icon = {
-                Icon(
-                    Icons.Filled.Person,
-                    contentDescription = null //TODO create string resources for titles
-                )
-            },
-            label = { Text("Text") } //TODO create string resources for labels
-        )
+        allScreens.forEach { screen ->
+            NavigationBarItem(
+                selected = currentScreen == screen,
+                onClick = { onTabSelected(screen) },
+                icon = { Icon(screen.icon, contentDescription = null) },
+                label = { Text(screen.label) }
+            )
+        }
     }
 }
 
@@ -66,11 +36,6 @@ fun AppNavBar(
 @Composable
 fun PreviewAppNavBar() {
     KindTheme {
-        AppNavBar(
-            currentRoute = KindDestinations.HOME_ROUTE,
-            navigateToHome = { },
-            navigateToSetPortfolio = { },
-            navigateToMyPage = { }
-        )
+        AppNavBar(allScreens = kindBottomBarScreens, onTabSelected = {}, currentScreen = Home)
     }
 }
