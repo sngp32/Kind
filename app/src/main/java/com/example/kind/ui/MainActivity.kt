fbedddb6e0e3f9fa3bff49342a5e2aa456040a3b
GoodGithubAccount <22419372+GoodGithubAccount@users.noreply.github.com>
2023-01-06 12:44:35 +0100

    Kindapp wasnt refactored??

--- app/src/main/java/com/example/kind/ui/MainActivity.kt
+++ app/src/main/java/com/example/kind/ui/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.interaction.DragInteraction
-import com.example.kind.KindApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
