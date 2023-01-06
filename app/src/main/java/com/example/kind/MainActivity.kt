2f8ab28d1dfb68fa4e6db62d212d9b1ead82e133
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-06 17:08:56 +0100

    Change MainActivity import and package
    MainActivity has been moved out of UI and unused imports have been removed.

--- app/src/main/java/com/example/kind/MainActivity.kt
+++ app/src/main/java/com/example/kind/MainActivity.kt
@@ -1,14 +1,10 @@
-package com.example.kind.ui
+package com.example.kind
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.interaction.DragInteraction
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import androidx.activity.viewModels
+import com.example.kind.ui.KindApp
 
 class MainActivity : ComponentActivity() {
 
