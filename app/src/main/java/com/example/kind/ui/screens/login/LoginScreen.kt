22820b8626674c9939c78e3524ffde675438a055
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-10 02:13:00 +0100

    Change login screen layout
    Major refactoring and layout simplification

--- app/src/main/java/com/example/kind/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/kind/ui/screens/login/LoginScreen.kt
@@ -1,93 +1,149 @@
 package com.example.kind.ui.screens.login
 
-import android.annotation.SuppressLint
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.kind.ui.components.navigation.Login
 import com.example.kind.ui.components.navigation.Signup
 
 @Composable
 fun LoginScreen(
     onLoginClick: (List<String>) -> Unit,
     onSignUpClick: () -> Unit,
-    onIncorrectLogin: () -> Unit, 
+    onIncorrectLogin: () -> Unit,
+    onResetPasswordClick: () -> Unit
 ) {
-    var textEmail = remember { mutableStateOf("") }
-    var textPassword = remember { mutableStateOf("") }
+    var inputEmail = remember { mutableStateOf("") }
+    var inputPassword = remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
     ) {
+        LoginHeader()
+
+        Spacer(modifier = Modifier.height(90.dp))
+
         Column(
             modifier = Modifier
-                .height(500.dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.Top
+                .fillMaxWidth()
+                .padding(horizontal = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "LOGIN",
-                fontSize = 48.sp,
-                modifier = Modifier.padding(start = 20.dp, top = 50.dp)
+            LoginInputFields(
+                inputEmail.value,
+                inputPassword.value,
+                { input -> inputEmail.value = input },
+                { input -> inputPassword.value = input }
             )
-            Text(
-                text = "Start your journey of giving",
-                fontSize = 28.sp,
-                color = Color.White,
-                modifier = Modifier.padding(start = 20.dp)
-            )
-            Spacer(modifier = Modifier.height(90.dp))
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                InputField(
-                    value = textEmail.value,
-                    label = "E-mail",
-                    onValueChange = { input -> textEmail.value = input })
-                Spacer(modifier = Modifier.height(10.dp))
-                InputField(
-                    value = textPassword.value,
-                    label = "Password",
-                    onValueChange = { input -> textPassword.value = input })
+            Spacer(Modifier.height(5.dp))
 
-                Button(navigation = {
-                    onLoginClick(
-                        listOf(
-                            textEmail.value,
-                            textPassword.value
-                        )
-                    )
-                }, text = "LOGIN")
-                ClickableText(
-                    text = AnnotatedString("Forgot your password?"),
-                    onClick = {  }
-                )
+            Row {
+                ResetPasswordButton(onResetPasswordClick)
+                Spacer(modifier = Modifier.weight(1f))
+                LoginButton(onLoginClick, inputEmail.value, inputPassword.value)
             }
         }
         Column(
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
+                .padding(horizontal = 50.dp)
                 .padding(bottom = 20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom
         ) {
-            Text(text = "Don't have an account?")
-            val signUpButton = Signup
-            Button(navigation = onSignUpClick, text = signUpButton.label)
+            Text(
+                text = "Don't have an account?",
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            SignUpButton(
+                onClick = onSignUpClick,
+                modifier = Modifier.width(300.dp)
+            )
         }
+    }
+}
+
+@Composable
+fun ResetPasswordButton(onResetPasswordClick: () -> Unit) {
+    TextButton(onClick = onResetPasswordClick) {
+        Text(text = "Reset password")
+    }
+}
+
+@Composable
+private fun SignUpButton(
+    onClick: () -> Unit,
+    modifier: Modifier
+) {
+    OutlinedButton(onClick = onClick, modifier) {
+        Text(text = Signup.label)
+    }
+}
+
+@Composable
+private fun LoginButton(
+    onLoginClick: (List<String>) -> Unit,
+    inputEmail: String,
+    inputPassword: String
+) {
+    Button(onClick = {
+        onLoginClick(
+            listOf(
+                inputEmail,
+                inputPassword
+            )
+        )
+    }) {
+        Text(text = Login.label)
+    }
+}
+
+@Composable
+private fun LoginInputFields(
+    inputEmail: String,
+    inputPassword: String,
+    onEmailInput: (String) -> Unit,
+    onPasswordInput: (String) -> Unit
+) {
+    InputField(
+        value = inputEmail,
+        label = "E-mail",
+        onValueChange = onEmailInput
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+    InputField(
+        value = inputPassword,
+        label = "Password",
+        onValueChange = onPasswordInput
+    )
+}
+
+@Composable
+private fun LoginHeader() {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.padding(start = 20.dp, top = 50.dp)
+    ) {
+        Text(
+            text = "LOGIN",
+            fontSize = 48.sp,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+        Text(
+            text = "Start your journey of giving",
+            fontSize = 28.sp,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+        )
     }
 }
 
@@ -105,10 +161,7 @@ private fun InputField(
         value = value,
         onValueChange = onValueChange,
         label = { Text(text = label) },
-        modifier = Modifier.width(300.dp),
-        colors = TextFieldDefaults.textFieldColors(
-            containerColor = Color.White
-        )
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
