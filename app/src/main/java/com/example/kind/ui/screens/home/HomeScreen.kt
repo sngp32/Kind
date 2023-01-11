edff8b35d9114ceba81bf1ebca0f95db6a0fb28e
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-01-11 01:22:42 +0100

    Remove predefined background color from Home
    With Material theme the app will default to the correct
    background.

--- app/src/main/java/com/example/kind/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/kind/ui/screens/home/HomeScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,7 +36,6 @@ fun HomeScreen(
             LazyColumn(
                 modifier = modifier
                     .fillMaxSize()
-                    .background(color = Color(0xffebebeb))
                     .padding(start = 25.dp, end = 25.dp, top = 15.dp),
                 verticalArrangement = Arrangement.spacedBy(20.dp)
             ) {
